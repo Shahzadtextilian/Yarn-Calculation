fd15a293ae49f5e35e94b4f595e0e27701fed061
Shahzadtextilian <shahzadtextilian@gmail.com>
2026-09-20 21:18:38 +0500

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2255748817" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "145554040" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
